508e2b33585389495bc0b7cb9525df5151373aba
uneeb125 <uneebkamal125@gmail.com>
2024-07-13 17:18:27 -0700

    added a bunch of lsp servers

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -9,6 +9,16 @@ M.servers = {
   "bashls",
   "jsonls",
   "yamlls",
+  "clangd",
+  "cmake",
+  "dockerls",
+  "docker_compose_language_service",
+  "golangci_lint_ls",
+  "gopls",
+  "ltex",
+  "texlab",
+  "rust_analyzer",
+  "svlangserver"
 }
 
 return M
