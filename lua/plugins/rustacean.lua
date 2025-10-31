fe4ecd6a7696a0757a24cd5fcf06721cdb3abe3e
uneeb125 <uneebkamal125@gmail.com>
2025-10-31 13:36:15 -0500

    Attempt rustanalyzer manual load, does not work

--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -2,4 +2,11 @@ return {
   'mrcjkb/rustaceanvim',
   version = '^5', -- Recommended
   lazy = false, -- This plugin is already lazy
+  config = function()
+    vim.g.rustaceanvim = {
+      server = {
+        autostart = false,
+      },
+    }
+  end,
 }
