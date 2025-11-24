9457c4cb3928f35822f1a8d91443bd8189327b70
uneeb125 <uneebkamal125@gmail.com>
2025-11-24 10:06:23 -0600

    Tree sitter auto install parsers

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -9,7 +9,7 @@ return {
         require("nvim-treesitter.configs").setup({
             ensure_installed = {},
             sync_install = false,
-            auto_install = false,
+            auto_install = true,
 
             highlight = {
                 enable = true,
