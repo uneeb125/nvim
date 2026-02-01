1c68429c4d45c86bb0167fc4ef85cd76a8524a94
uneeb125 <uneebkamal125@gmail.com>
2026-02-01 16:21:28 -0600

    Tried adding colorizer, didn't work. Autopair works though

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,7 +1,9 @@
-local M = {
-    "norcalli/nvim-colorizer.lua",
-    commit = "a065833f35a3a7cc3ef137ac88b5381da2ba302e",
-    event = "BufReadPost",
+return {
+    "NvChad/nvim-colorizer.lua",
+    event = "VeryLazy",
+    opts = {
+        "", -- Highlight all files, but customize some others.
+        "!vim", -- Exclude vim from highlighting.
+        -- Exclusion Only makes sense if '' is specified!
+    },
 }
-
-return M
