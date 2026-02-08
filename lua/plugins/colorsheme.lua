58722a1383f5bfa74e2730e27525e42d3a37f48c
uneeb125 <uneebkamal125@gmail.com>
2026-02-07 18:14:31 -0600

    Make the background black

--- lua/plugins/colorsheme.lua
+++ lua/plugins/colorsheme.lua
@@ -120,6 +120,18 @@ return {
 
         -- This command now uses the compiled Lua theme, which is extremely fast.
         vim.cmd.colorscheme("catppuccin")
+
+        vim.opt.termguicolors = true
+        vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+        vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
+
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            pattern = "*",
+            callback = function()
+                vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+                vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
+            end,
+        })
     end,
 }
 
