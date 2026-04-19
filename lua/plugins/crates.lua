ef2a16234fa866d729e5e334734591bd136ce5a9
uneeb125 <uneebkamal125@gmail.com>
2026-04-19 14:18:13 -0500

    Made crate.lua a lot more usefull

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -3,7 +3,10 @@ return {
     tag = "stable",
     event = { "BufRead Cargo.toml" },
     config = function()
-        require("crates").setup({
+        local crates = require("crates")
+        local opts = { silent = true }
+
+        crates.setup({
             smart_insert = true,
             remove_enabled_default_features = true,
             remove_empty_features = true,
@@ -145,6 +148,21 @@ return {
                     prerelease = " pre-release ",
                     yanked = " yanked ",
                 },
+                blink = {
+                    use_custom_kind = true,
+                    kind_text = {
+                        version = "Version",
+                        feature = "Feature",
+                    },
+                    kind_highlight = {
+                        version = "BlinkCmpKindVersion",
+                        feature = "BlinkCmpKindFeature",
+                    },
+                    kind_icon = {
+                        version = " ",
+                        feature = " ",
+                    },
+                },
                 crates = {
                     enabled = true,
                     min_chars = 0,
@@ -160,5 +178,29 @@ return {
                 hover = true,
             },
         })
+
+        vim.keymap.set("n", "<leader>ct", crates.toggle, { desc = "Crates: Toggle UI" })
+        vim.keymap.set("n", "<leader>cr", crates.reload, { desc = "Crates: Reload/Clear Cache" })
+
+        vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, { desc = "Crates: Versions Popup" })
+        vim.keymap.set("n", "<leader>cf", crates.show_features_popup, { desc = "Crates: Features Popup" })
+        vim.keymap.set("n", "<leader>cd", crates.show_dependencies_popup, { desc = "Crates: Dependencies Popup" })
+
+        vim.keymap.set("n", "<leader>cu", crates.update_crate, { desc = "Crates: Update Crate (Compatible)" })
+        vim.keymap.set("v", "<leader>cu", crates.update_crates, { desc = "Crates: Update Selected Crates" })
+        vim.keymap.set("n", "<leader>ca", crates.update_all_crates, { desc = "Crates: Update All Crates" })
+        vim.keymap.set("n", "<leader>cU", crates.upgrade_crate, { desc = "Crates: Upgrade Crate (Newest)" })
+        vim.keymap.set("v", "<leader>cU", crates.upgrade_crates, { desc = "Crates: Upgrade Selected Crates" })
+        vim.keymap.set("n", "<leader>cA", crates.upgrade_all_crates, { desc = "Crates: Upgrade All Crates" })
+
+        vim.keymap.set("n", "<leader>cx", crates.expand_plain_crate_to_inline_table, { desc = "Crates: Expand to Inline Table" })
+        vim.keymap.set("n", "<leader>cX", crates.extract_crate_into_table, { desc = "Crates: Extract into Table" })
+        vim.keymap.set("n", "<leader>cg", crates.use_git_source, { desc = "Crates: Use Git Source" })
+
+        vim.keymap.set("n", "<leader>cH", crates.open_homepage, { desc = "Crates: Open Homepage" })
+        vim.keymap.set("n", "<leader>cR", crates.open_repository, { desc = "Crates: Open Repository" })
+        vim.keymap.set("n", "<leader>cD", crates.open_documentation, { desc = "Crates: Open Documentation" })
+        vim.keymap.set("n", "<leader>cC", crates.open_crates_io, { desc = "Crates: Open crates.io" })
+        vim.keymap.set("n", "<leader>cL", crates.open_lib_rs, { desc = "Crates: Open lib.rs" })
     end,
 }
