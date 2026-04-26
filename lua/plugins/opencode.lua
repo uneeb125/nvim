1dd136baeadf9724755bf42d354ac8d70030cd06
uneeb125 <uneebkamal125@gmail.com>
2026-04-26 15:00:23 -0500

    opencode api not compatible with neovim v0.12

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -1,1 +1,65 @@
-return {}
+return {
+	"nickjvandyke/opencode.nvim",
+	version = "*",
+	dependencies = {
+		{
+			"folke/snacks.nvim",
+			opts = {
+				input = {},
+				picker = {
+					actions = {
+						opencode_send = function(...)
+							return require("opencode").snacks_picker_send(...)
+						end,
+					},
+					win = {
+						input = {
+							keys = {
+								["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+	config = function()
+		vim.g.opencode_opts = {
+			events = {
+				permissions = {
+					edits = {
+						enabled = false,
+					},
+				},
+			},
+		}
+		vim.o.autoread = true
+		-- Toggle
+		vim.keymap.set({ "n", "t" }, "<leader>og", function()
+			require("opencode").toggle()
+		end, { desc = "Toggle opencode" })
+		-- Ask / Open input
+		vim.keymap.set({ "n", "x" }, "<leader>oi", function()
+			require("opencode").ask("@this: ", { submit = true })
+		end, { desc = "Ask opencode" })
+		-- Visual selection
+		vim.keymap.set("v", "<leader>oy", function()
+			return require("opencode").operator("@this ")
+		end, { desc = "Add visual selection to opencode", expr = true })
+		-- Quick chat
+		vim.keymap.set({ "n", "x" }, "<leader>o/", function()
+			require("opencode").ask("@this: ", { submit = true })
+		end, { desc = "Quick chat" })
+		-- Select / Context
+		vim.keymap.set("n", "<leader>gl", function()
+			require("opencode").select()
+		end, { desc = "Opencode select" })
+		-- Scroll up/down
+		vim.keymap.set("n", "<S-C-u>", function()
+			require("opencode").command("session.half.page.up")
+		end, { desc = "Scroll opencode up" })
+		vim.keymap.set("n", "<S-C-d>", function()
+			require("opencode").command("session.half.page.down")
+		end, { desc = "Scroll opencode down" })
+	end,
+}
