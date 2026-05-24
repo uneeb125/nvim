4fd002d13e8f1e45834700b8ffdcfce46b5b75c0
uneeb125 <uneebkamal125@gmail.com>
2026-05-23 19:11:57 -0500

    somehow s wasn't working

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -68,6 +68,7 @@ return {
     },
     -- stylua: ignore
     keys = {
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
         { "f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
         { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
         { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
