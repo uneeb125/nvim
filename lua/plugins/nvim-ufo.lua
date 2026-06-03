7ea35d95781f2b9736c900cc357bffc6d929967b
uneeb125 <uneebkamal125@gmail.com>
2026-06-03 18:22:19 -0500

    made folds actually useful.

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -2,6 +2,15 @@ return {
     "kevinhwang91/nvim-ufo",
     dependencies = "kevinhwang91/promise-async",
     config = function()
-        require('ufo').setup()
-    end
+        vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+        vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+        vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+        vim.keymap.set("n", "zm", require("ufo").closeFoldsWith)
+
+        require("ufo").setup({
+            provider_selector = function(bufnr, filetype, buftype)
+                return { "treesitter", "indent" }
+            end,
+        })
+    end,
 }
